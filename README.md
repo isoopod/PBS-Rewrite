d72d523ca06c31c7ca46c4c924685cf96bb5b853
isoopod <isoopod@proton.me>
2025-12-07 12:41:50 +1300

    dev: change tooling
    moving from aftman and wally to rokit and pesde

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PBS-Rewrite
-# REWRITE IS CURRENTLY UNDER CONSTRUCTION ![Construction.gif](https://media.tenor.com/N1Hgl4qMR30AAAAM/mxstery.gif)
+
+**REWRITE IS CURRENTLY UNDER CONSTRUCTION** ![Construction.gif](https://media.tenor.com/N1Hgl4qMR30AAAAM/mxstery.gif)
 
 Contains the [documentation](https://github.com/isoopod/PBS-Rewrite/wiki) for working with Personal Build Server: Rewrite, as well as hosting stable releases
--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,3 +1,8 @@
+param(
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$PesdeArgs
+)
+
 # We want to make sure Rojo is not Live-Syncing because it will cause an error
 $port = 34872 # Default Rojo port
 $listener = Get-NetTCPConnection -LocalPort $port -ErrorAction SilentlyContinue
@@ -6,13 +11,9 @@ if ($listener -and $listener.State -eq "Listen") {
     Write-Warning "Stop your Rojo Live-Sync server before running this script."
 }
 else {
-    wally install
-
-    rojo sourcemap --output sourcemap.json default.project.json
-
-    wally-package-types --sourcemap sourcemap.json Packages DevPackages
+    pesde install @PesdeArgs
     
-    $testezPath = "Packages\_Index\roblox_testez@0.4.1"
+    $testezPath = "roblox_packages\.pesde\roblox_testez@0.4.1"
     if (Test-Path $testezPath) {
         Remove-Item -Recurse -Force $testezPath
         Write-Host "Removed roblox_testez@0.4.1 from Packages/__Index."
@@ -25,5 +26,7 @@ else {
     $reactContents = "-- selene: allow(global_usage)`n_G.__DEV__ = game:GetService('RunService'):IsStudio()`n"
     $reactContents = $reactContents + (Get-Content -Path .\Packages\React.lua -Encoding ASCII -Raw)
 
-    Set-Content -Path .\Packages\React.lua -Value $reactContents -Encoding ASCII
+    Set-Content -Path .\roblox_packages\React.lua -Value $reactContents -Encoding ASCII
+
+    rojo sourcemap --output sourcemap.json default.project.json
 }
